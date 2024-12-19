e86bd9fbbd88f12a597212052a5c59ce7214e28e
davidpinoP <pino7david@gmail.com>
2024-12-19 16:44:12 +0100

    inicio
    creacion de la matriz y rellenada con numeros aleatorios entre 1 y 100

--- src/git2/Git2.java
+++ src/git2/Git2.java
@@ -1,10 +1,19 @@
 package git2;
 
+import java.util.Random;
+
 public class Git2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Random Rn = new Random();
+		int[][] tablero = new int[5][5];
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				tablero[i][j] = Rn.nextInt(100) + 1;
+			}
+		}
 	}
 
 }
